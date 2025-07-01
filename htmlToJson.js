655f7ac41e373a2e7a00fb4b0b53d67c5da18536
bobonimo <carlosmaycon443@gmail.com>
2025-07-01 11:17:19 -0300

    ta andando

--- htmlToJson.js
+++ htmlToJson.js
@@ -1,31 +1,83 @@
-import fs from "node:fs"
 import * as cheerio from "cheerio";
-
-function htmlToJson(entrada) {
-    fs.readFile(entrada, "utf-8", (err, data) => {
-        if (err) throw err;
-        // Aq começamos a brincadeira
-        const $ = cheerio.load(data);
-
-        let xpath = "a[name]";
-        let capturas = $(xpath)
-
-        let objeto = {};
-        let ultimo_titulo = 0;
-        let ultimo_capitulo = 0;
-        let ultimo_artigo = 0;
-        let ultimo_paragrafo = 0;
+import fs from "node:fs"
 
 
-        capturas.each((index, el) => {
-            let parent = el.parent
-            if ($(parent).find("a").attr("name").substring(0, 6) == "titulo" && $(parent).find("a").attr("name").length <= 9) {
-                console.log($(parent).html())
-            }
+function htmlToJson(html) {
 
-        })
 
-    });
+    // Aq começamos a brincadeira
+    const $ = cheerio.load(html);
+
+    let capturas = $("body").find("p, div, blockquote")
+
+    let objeto = {};
+    let ultimo_titulo = 0;
+    let ultimo_capitulo = 0;
+    let ultimo_artigo = 0;
+    let ultimo_paragrafo = 0;
+
+    // console.log($(capturas.get(4)).html())
+
+    capturas.each((index, el) => {
+        if ($(el).find("strike") > 0) {
+            return null;
+        }
+
+        const nome = $(el).html() || "";
+        // Titulo
+        if (new RegExp("\\bt.tulo\\s*[ivxlcdm]+\\b", "i").test(nome)
+            // && !(new RegExp("capitulo[i,v,x]").test($(el).text()))
+
+        ) {
+            ultimo_titulo = $(el)
+            let titulo_encontrado = $(el);
+            // console.log("dectado: " + titulo_encontrado.trim())
+            objeto[titulo_encontrado.find("name").first().attr("name")] = {};
+            writeFile(titulo_encontrado.find("[name]").first().attr("name"));
+
+        }
+        // Artigo 
+        else if ($(el).attr("id") == "art") {
+
+            let bloco_de_artigos = $(el).find("p");
+            let ultimo_artigo_indexador = 0;
+            // o^2
+            bloco_de_artigos.each((index, art) => {
+
+                //econtrar artigos
+                if (new RegExp("Art.").test($(art).text())) {
+                    ultimo_artigo = $(art)
+                    ultimo_artigo_indexador = ultimo_artigo.find("a").first().attr("[name]");
+                    objeto[ultimo_titulo][ultimo_artigo_indexador] =
+                        { "text": ultimo_artigo.text(), "paragrafos": new Array() }
+                }
+                else if (new RegExp(`\\bt${ultimo_artigo.find("a").first().attr("[name]")}\\s*[ivxlcdm]+\\b`)
+                    .test($(art).find("[name]").first().attr("name"))) {
+
+                    objeto[ultimo_capitulo][ultimo_artigo_indexador]["paragrafos"].append($(art).text());
+                }
+
+            })
+
+
+            // console.log(artigo_encontrado.text());
+            // writeFile(artigo_encontrado.text());
+        }
+        console.log(objeto);
+        // pargrafos
+    })
+
+
 }
+
+function regex_teste(texto, palavra) {
+    const regex = new RegExp(`\\b${palavra}\\b`, 'i');
+    return regex.test(texto);
+}
+
+function writeFile(text) {
+    fs.appendFileSync("saida.txt", text + "\n");
+}
+
 
 export default htmlToJson;
--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import htmlToJson from "./htmlToJson.js"
+import fs from "node:fs"
 
-htmlToJson("./read_archives/entrada.html")
+import axios from "axios"
+
+// axios.get("https://www.planalto.gov.br/ccivil_03/constituicao/constituicao.htm", {
+//     headers: {
+//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+//         "Accept": "text/html",
+//         "Accept-Language": "pt-BR,pt;q=0.9"
+//     }, timeout: 10000
+// })
+//     .then(function (response) {
+//         console.log(response);
+//     }).catch(err => {
+//         console.log(err);
+//     })
+
+fs.readFile("./read_archives/entrada.html", "utf-8", (err, data) => {
+    if (err) throw err;
+    htmlToJson(data);
+})
